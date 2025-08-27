be894b4e2dd5d567d20af7d89e07c0df36448b7b
sameerthakur12 <thakursameer1619@gmail.com>
2025-08-27 10:34:41 +0530

    feat: add URL shortener core logic with Express routes, controller, and MongoDB model

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const urlRoute = require("./routes/url");
+const app = express();
+const PORT = 8000;
+
+app.use("/url", urlRoute);
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
