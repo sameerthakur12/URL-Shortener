d07356c3001dead47a2bb7fc885a503fcddcd68d
sameerthakur12 <thakursameer1619@gmail.com>
2025-08-29 10:06:34 +0530

    Improved user experience by allowing browser-based shortURL creation

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ const express = require("express");
 const app = express();
 const {connectToMongoDB} = require("./connect");
 const URL = require("./models/url");
+const path = require("path");
+const staticRoute = require("./routes/staticRouter");
 
 const urlRoute = require("./routes/url");
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
 const PORT = 8001;
 
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() => console.log("MongoDB connected"));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
+app.get("/", async(req, res) => {
+    const allURLs = await URL.find({});
+    return res.render("home", {
+        urls: allURLs,
+    });
+})
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
